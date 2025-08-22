a64e6e4255ab7467dfd796847d502f537b5fd4ea
yohan-sketch <bryanyohan02@gmail.com>
2025-08-22 19:56:58 +0700

    Add files via upload

---
+++ cv_web/portfolio/projects-content.php
@@ -0,0 +1,114 @@
+<section id="projects" class="bg-white shadow rounded-xl p-6">
+  <h2 class="text-2xl font-bold mb-6">Projects</h2>
+  <div class="grid sm:grid-cols-2 gap-6">
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/1_graphic-cv_23juli.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Graphic CV</h3>
+        <p class="text-sm text-gray-600 mb-2">Create a graphic CV with Canva.</p>
+        <a href="https://drive.google.com/file/d/1dSI_v0B84uQ2BF4oE_yMK6Kn2E66ZEuB/view?usp=drive_link" class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/2_webCV_24juli.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Web CV</h3>
+        <p class="text-sm text-gray-600 mb-2">Create an CV with Tailwind CSS.</p>
+        <a href="https://yohan-sketch.github.io/Web_CV_Febryan-Yohan-Saputra_24-July/"class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/3_Animated CV_25juli.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Animated CV</h3>
+        <p class="text-sm text-gray-600 mb-2">Create an Animated CV with Canva.</p>
+        <a href="https://drive.google.com/file/d/1zC_XjD3s3kGwlQvNBfq4NbVD5nZ51Oc6/view?usp=sharing"
+        class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/4_initial logo_28juli.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">initial logo</h3>
+        <p class="text-sm text-gray-600 mb-2">Create 3 Concepts Logo Design.</p>
+        <a href="https://drive.google.com/drive/folders/1I7CwFdXLupRlkBSqCmO_o0qORTbaevkk?usp=drive_link"
+        class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/5_video profile_29juli.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Animated CV</h3>
+        <p class="text-sm text-gray-600 mb-2">Create an Animated CV with Canva.</p>
+        <a href="https://drive.google.com/file/d/1-5rdc_1miozYjvIJ28ywhov7UGe8orOG/view?usp=drive_link"
+        class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/6_web SMA 2 kota magelang(30juli-1agustus).png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Web Design</h3>
+        <p class="text-sm text-gray-600 mb-2">Create Web Design for SMAN2 Magelang with Figma.</p>
+        <a href="https://drive.google.com/file/d/1nVUojSal6RksQWZs6R9NBWCiSbjjBZR0/view?usp=drive_link"
+        class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/7_instagram design mie setang.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Template Instagram</h3>
+        <p class="text-sm text-gray-600 mb-2">Create an Template For Instagram with Canva.</p>
+        <a href="https://drive.google.com/drive/folders/1fU8ScPlyYtNzUm5ty1Yns7aZu0oJRWzx?usp=sharing"
+        class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/7_instagram design tempat biasa_insta.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Template Instagram</h3>
+        <p class="text-sm text-gray-600 mb-2">Create an Template For Instagram with Canva.</p>
+        <a href="https://drive.google.com/drive/folders/1aAZ3GS96VDLwGIrqrW9vM_ITVhi_api4?usp=drive_link"
+        class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/8_design web bipa_8-12agustus.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Design Web for BIPA</h3>
+        <p class="text-sm text-gray-600 mb-2">Create a Design Web For BIPA with Figma.</p>
+        <a href="https://drive.google.com/file/d/12wC5waaR1pnk7y1H2GhlkjmXsyKmgee_/view?usp=drive_link"
+        class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/9_design web Develop  with Atlas_13-15agustus.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Design Web For "Develop with Atlas" Company</h3>
+        <p class="text-sm text-gray-600 mb-2">Create a Design Web For "Develop with Atlas" Company with Figma.</p>
+        <a href="https://drive.google.com/file/d/1DN7uHNzXjCi_6dvou8Vf6Aqnq7PcNrL6/view?usp=drive_link"
+        class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+    <div class="bg-gray-50 border rounded-xl overflow-hidden">
+      <img src="portfolio/10_secure service agreement form_19-21agustus.png" alt="project" class="w-full h-40 object-cover">
+      <div class="p-4">
+        <h3 class="font-semibold">Secure Service Agreement Form</h3>
+        <p class="text-sm text-gray-600 mb-2">Create secure service agreement form using PHP.</p>
+        <a href="https://drive.google.com/drive/folders/14RvCC3LuQw5INhNMHJnuyF0deEOJzFGc?usp=drive_link"
+        class="text-blue-600 hover:underline text-sm">View</a>
+      </div>
+    </div>
+
+  </div>
+</section>
